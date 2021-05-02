fb42f6813f655cc22d1153449916bccfdd1f763c
emzubair <muhammad.zubair@arbisoft.com>
2021-05-02 14:35:24 +0500

    Added APIView with get, put, patch, post & delete methods. Simplre serializer aded

---
+++ profile_api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+
+class HelloSerializers(serializers.Serializer):
+    """Hello API serializers """
+
+    name = serializers.CharField(max_length=20)
+    password = serializers.CharField(
+        max_length=100, style={'input_type': 'password'})
