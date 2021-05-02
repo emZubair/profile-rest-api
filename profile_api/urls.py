b9d69b866a85076f64ab18be98420ca267f51df3
emzubair <muhammad.zubair@arbisoft.com>
2021-05-02 15:08:36 +0500

    Added a sample viewset with Default router and supported CRUD operations

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from profile_api.views import HelloApiView
+from django.urls import path, include
+from profile_api.views import HelloApiView, HelloViewsets
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset', HelloViewsets, basename='hello-viewset')
 
 urlpatterns = [
     path('hello', HelloApiView.as_view(), name='hello'),
+    path('', include(router.urls)),
 ]
