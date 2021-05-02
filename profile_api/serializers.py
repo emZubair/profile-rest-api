85352782d4e5bb604af81b4011771d7566a672e4
emzubair <muhammad.zubair@arbisoft.com>
2021-05-02 16:18:21 +0500

    Added model serializer for User profile model

--- profile_api/serializers.py
+++ profile_api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from profile_api.models import UserProfile
 
 
 class HelloSerializers(serializers.Serializer):
@@ -7,3 +8,35 @@ class HelloSerializers(serializers.Serializer):
     name = serializers.CharField(max_length=20)
     # password = serializers.CharField(
     # max_length=100, style={'input_type': 'password'})
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """ Serializer to serialize UserProfile models objects """
+
+    class Meta:
+        model = UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """ create and return new user """
+
+        user = UserProfile.objects.create_user(
+            email=validated_data.get('email'),
+            name=validated_data.get('name'),
+            password=validated_data.get('password')
+        )
+
+    def update(self, instance, validated_data):
+        """ Handle updating user account """
+
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
--- profile_api/urls.py
+++ profile_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from profile_api.views import HelloApiView, HelloViewsets
+from profile_api.views import HelloApiView, HelloViewsets, UserProfileViewset
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('hello-viewset', HelloViewsets, basename='hello-viewset')
+router.register('profile', UserProfileViewset)
 
 urlpatterns = [
     path('hello', HelloApiView.as_view(), name='hello'),
