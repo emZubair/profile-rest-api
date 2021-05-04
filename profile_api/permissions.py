77935f735725389df300aab9af708b8a0cf56504
emzubair <muhammad.zubair@arbisoft.com>
2021-05-05 00:49:12 +0500

    API view permissions added

--- profile_api/permissions.py
+++ profile_api/permissions.py
@@ -8,3 +8,18 @@ class UpdateUserProfilePermission(permissions.BasePermission):
         """ Check if user is trying to update their own profile """
 
         return request.method in permissions.SAFE_METHODS or obj.id == request.user.id
+
+
+class UpdateOwnStatusPermission(permissions.BasePermission):
+    """ Allow users to update their status """
+
+    def has_permission(self, request, view):
+        """
+        Return `True` if permission is granted, `False` otherwise.
+        """
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        """ Check user is updating their own status """
+
+        return request.method in permissions.SAFE_METHODS or obj.user_profile.id == request.user.id
